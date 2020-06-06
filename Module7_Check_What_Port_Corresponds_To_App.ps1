679063f07177e73280a7f88a654b5957d83a7f1f
powershell4support <66219007+powershell4support@users.noreply.github.com>
2020-06-06 20:07:16 +0100

    Module 7. Check Ports VS Process

---
+++ Module7_Check_What_Port_Corresponds_To_App.ps1
@@ -0,0 +1,41 @@
+﻿CLEAR
+Write-host ""
+Write-host "______                          _          _ _     ___   _____                              _   " -ForegroundColor Cyan
+Write-host "| ___ \                        | |        | | |   /   | /  ___|                            | |  " -ForegroundColor Cyan
+Write-host "| |_/ /____      _____ _ __ ___| |__   ___| | |  / /| | \ `--. _   _ _ __  _ __   ___  _ __| |_ " -ForegroundColor Cyan
+Write-host "|  __/ _ \ \ /\ / / _ \ '__/ __| '_ \ / _ \ | | / /_| |  `--. \ | | | '_ \| '_ \ / _ \| '__| __|" -ForegroundColor Cyan
+Write-host "| | | (_) \ V  V /  __/ |  \__ \ | | |  __/ | | \___  | /\__/ / |_| | |_) | |_) | (_) | |  | |_ " -ForegroundColor Cyan
+Write-host "\_|  \___/ \_/\_/ \___|_|  |___/_| |_|\___|_|_|     |_/ \____/ \__,_| .__/| .__/ \___/|_|   \__|" -ForegroundColor Cyan
+Write-host "                                                                    | |   | |                   " -ForegroundColor Cyan
+Write-host "                                                                    |_|   |_|                   " -ForegroundColor Cyan
+Write-host "Developed by David Mena" -ForegroundColor Cyan
+Write-Host "PLEASE SUBSCRIBE : https://www.youtube.com/channel/UCCJlFicsPdVld48G0ApdUJw/"
+Write-host ""
+Write-host "MODULE 7. CHECK WHAT PORT CORRESPONDS TO WHAT APPLICATION" -ForegroundColor Green
+Write-host ""
+Write-host ""
+
+# Read Host to wait for an Input
+    $Port=Read-host "Type the port number to query "
+    $Q2=Read-host "Do you want to export the result to CSV? (Y/N) "
+# Query using the input
+    $check_port=get-nettcpconnection |where-object {$_.remoteport -eq $Port}| select local*,remote*,state,@{Name="Process";Expression={(Get-Process -Id $_.OwningProcess).ProcessName}} 
+    $check_port |ft *
+# Conditional in case you have selected Y TO CSV
+    If ($Q2 -eq "Y")
+{
+    $test=Test-Path C:\scripts
+# Check if the output folder exists, if not, creates it.
+    If ($test -eq $false)
+{
+    mkdir C:\scripts
+}
+    If ($test -eq $true)
+{
+# Remove old files just in case there is one
+    rm c:\scripts\Port_Query.csv -Force
+# Export Output to a CSV
+    $check_port |export-csv c:\scripts\Port_Query.csv -NoTypeInformation -Append
+}
+}
+
